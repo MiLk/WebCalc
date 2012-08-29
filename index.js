973c50f80ad35e28ca32ceac4546d78a0e2a0889
MiLk <hello@emilienkenler.com>
2012-08-29 14:27:13 +0300

    Result in text/html

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ var calc = function (arithmetic) {
 };
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url,true).query.q;
   var result  = calc(q);
   console.log('Query: '+q+' - Result: '+result);
-  res.end(result+'');
+  res.end('<html><head><title>WebCalc</title><head><body><p>'+result+'</p></body></head>');
 }).listen(3000);
 
 console.log('Webcalc is running');
